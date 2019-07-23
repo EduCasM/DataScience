244f365257cfb9d07a3c1d09f7aac6f36a36c280
EduCasM <53205307+EduCasM@users.noreply.github.com>
2019-07-22 23:01:54 -0500

    Add files via upload
    add the init file to make this directory a python package

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo necesario para que el directorio "DataScience" sea un 
+un paquete de python.
+
+"""
+
+print("Bienvenido, Está iniciando la paquetería para Data science de EduCM")
