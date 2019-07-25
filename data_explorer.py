b2af8c340ff80a7462e09c8722719f62e86b96a1
EduCasM <edu_30sec@hotmail.com>
2019-07-25 00:18:37 -0500

    modified the structure of the imports

--- data_explorer.py
+++ data_explorer.py
@@ -4,12 +4,13 @@ Created on Thu Jul 18 21:49:13 2019
 
 @author: EduCM
 """
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def quality_report(data):
-    
+
     """This method will do a basic data quality report for a data frame"""
-    import pandas as pd
-    
+        
     if (type(data) != pd.core.frame.DataFrame):
         raise TypeError("Data must be pandas.core.frame.DataFrame")
     else: 
@@ -39,9 +40,8 @@ def get_histogram(data,column,bins):
     
     """This method will return a matplotlib.pyplot object Histogram from a passed
     column of a pandas Dataframe"""
-    import pandas as pd
-    import matplotlib.pyplot as plt
     
+        
     if (type(data) != pd.core.frame.DataFrame):
         raise TypeError("Data must be pandas.core.frame.DataFrame")
     
@@ -57,19 +57,17 @@ def get_histogram(data,column,bins):
         plt.title('Histogram of column:' + column)
         plt.xlabel(column)
         plt.ylabel('Frequency')
-        return(plt)
+        return plt
 
 def get_correlation_plot(data,columns_index,plot_size):
-    """this method will plot the correlation for the columns_index list of
+    """this method will return a matplotlib.pyplot object with the correlation for the columns_index list of
     a pandas DataFrame"""
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    
+        
     if (type(data) !=pd.core.frame.DataFrame):
         raise TypeError("Data must be pandas.core.frame.DataFrame")
     
     elif( type(columns_index) != list):
-        raise TypeError("columns_index must be a list")
+        raise iipeError("columns_index must be a list")
     
     elif(all(isinstance(n, int) for n in columns_index) == False):
         raise TypeError("elements of columns_index must be int type")
@@ -84,6 +82,6 @@ def get_correlation_plot(data,columns_index,plot_size):
         corr = data.iloc[:,columns_index].corr()
         fig,ax = plt.subplots(figsize=(plot_size, plot_size))
         ax.matshow(corr)
-        plt.xticks(range(len(corr.columns)), corr.columns,fontsize=30)
-        plt.yticks(range(len(corr.columns)), corr.columns,fontsize=30)
-        return plt.show()
+        plt.xticks(range(len(corr.columns)), corr.columns,fontsize=20)
+        plt.yticks(range(len(corr.columns)), corr.columns,fontsize=20)
+        return plt
